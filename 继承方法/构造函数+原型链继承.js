6288cf611c64da1f3d6abe7d0952462ec5a1e710
ncyu <naichunyu@aliyun.com>
2017-03-01 17:48:19 +0800

    modify

--- 继承方法/构造函数+原型链继承.js
+++ 继承方法/构造函数+原型链继承.js
@@ -13,11 +13,11 @@ SuperType.prototype.sayName=function(){
 	console.log(this.name);
 };
 function SubType(name,age){
-	//继承实例属性
+	//call指的是在（未来将要）新创建的SubType实例的环境中调用SuperType构造函数【相当于传递实例参数】
 	SuperType.call(this,name);
 	this.age=age;
 }
-//继承原型上的方法
+//继承原型上的方法【也会继承name、colors属性】
 SubType.prototype=new SuperType();
 //自己特有方法
 SubType.prototype.sayAge=function(){
@@ -31,5 +31,5 @@ instance.sayAge();//29
 
 var instance1=new SubType("lisi",27);
 console.log(instance1.colors);//"red,blue,green"
-instance.sayName();//"lisi"
-instance.sayAge();//27
+instance1.sayName();//"lisi"
+instance1.sayAge();//27
