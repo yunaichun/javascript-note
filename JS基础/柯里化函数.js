640aac5a1d2d415e54921b0a5544aeac49b95c5b
ncyu <naichunyu@aliyun.com>
2017-03-12 15:34:04 +0800

    柯里化函数实现类似add(a,b,c)=currying(add)(1)(2)(3)

--- JS基础/柯里化函数.js
+++ JS基础/柯里化函数.js
@@ -1,29 +1,64 @@
+/**
+ * 例：实现add(1,2,3)=currying(add)(1)(2)(3)
+ *     或者add(1,2,3)=currying(add,1)(2)(3)
+ *     或者add(1,2,3)=currying(add,1)(2,3)
+ *     或者add(1,2,3)=currying(add,1,2)(3)
+ *     或者add(1,2,3)=currying(add,1,2,3)
+ */
+function add(a,b,c){
+  return a+b+c;
+}
 /*柯里化函数：参数无回调函数*/
 function currying(fn) {
-	//截取第一个参数之外的所有参数
     var out = Array.prototype.slice.call(arguments, 1);
+    // add(1,2,3)=currying(add,1,2,3)
+    if(out.length==3){
+        return fn.apply(this, out);
+    }
+    //add(1,2,3)=currying(add,1,2)(3)
+    if(out.length==2){
+        return function(){
+          var inner1 = Array.prototype.slice.call(arguments);
+          return fn.apply(this, out.concat(inner1));
+        };
+    }
+    //add(1,2,3)=currying(add,1)(2,3)或者add(1,2,3)=currying(add,1)(2)(3)
+    if(out.length==1){
+      return function(){
+        var inner1 = Array.prototype.slice.call(arguments);
+        //add(1,2,3)=currying(add,1)(2,3)
+        if(inner1.length==2){
+          return fn.apply(this, out.concat(inner1));
+        }
+        //add(1,2,3)=currying(add,1)(2)(3)
+        if(inner1.length==1){
+          return function(){
+              var inner2 = inner1.concat(Array.prototype.slice.call(arguments));
+              return fn.apply(this, out.concat(inner2));
+          };
+        }
+      };
+    }
+    //add(1,2,3)=currying(add)(1)(2)(3)
     return function () {
-    	//截取所有的参数
-        var inner = Array.prototype.slice.call(arguments);
-        //调用fn，传入所有参数
-        return fn.apply(null, out.concat(inner));
+        var inner1 = Array.prototype.slice.call(arguments);
+        console.log(inner1);
+        return function(){
+            var inner2 = inner1.concat(Array.prototype.slice.call(arguments));
+            console.log(inner2);
+            return function(){
+              var inner3 = inner2.concat(Array.prototype.slice.call(arguments));
+              console.log(inner3);
+              return fn.apply(this, out.concat(inner3));
+            };
+        };
     };
 }
-function add(a,b,c){
-	return a+b+c;
-}
-console.log(currying(add,4)(1,2,3));//7
-
-
-
-//在此理解   闭包.js
-function make_pow(n){
-	return function(x){
-		return Math.pow(x,n);
-	}
-}
-var pow2=make_pow(2);
-console.log(pow2(5));//console.log(make_pow(2)(3))
+console.log(currying(add)(1)(2)(3));//6
+console.log(currying(add,1)(2)(3));//6
+console.log(currying(add,1)(2,3));//6
+console.log(currying(add,1,2)(3));//6
+console.log(currying(add,1,2,3));//6
 
 
 
@@ -35,7 +70,7 @@ var Thunk = function(fn){
     return function (callback){
       args.push(callback);
       return fn.apply(this, args);
-    }
+    };
   };
 };
 // ES6版本：参数有回调函数
@@ -43,7 +78,7 @@ var Thunk = function(fn) {
   return function (...args) {
     return function (callback) {
       return fn.call(this, ...args, callback);//call传入多参数
-    }
+    };
   };
 };
 // 使用上面的转换器，生成fs.readFile的 Thunk 函数。
@@ -55,4 +90,15 @@ function f(a, cb) {
 }
 let ft = Thunk(f);
 let log = console.log.bind(console);//bind绑定到console对象
-ft(1)(log) // 1
+ft(1)(log); // 1
+
+
+
+//再次理解:/闭包/闭包作用及注意点.js
+function make_pow(n){
+  return function(x){
+    return Math.pow(x,n);
+  };
+}
+var pow2=make_pow(2);
+console.log(pow2(5));//console.log(make_pow(2)(3))
