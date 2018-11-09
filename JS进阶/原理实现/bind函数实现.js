6f653ebe8d9747ba8a52a8fa8a01a24fa59c95bb
余乃春 <yunaichun@hikvision.com.cn>
2018-11-09 15:35:08 +0800

    bind函数使用new操作符之后this不指向参数对象，也不指向window，而是指向返回的函数

--- JS进阶/原理实现/bind函数实现.js
+++ JS进阶/原理实现/bind函数实现.js
@@ -5,29 +5,42 @@
  */
 Function.prototype.fakeBind = function (context) {
     let self = this;
-    // 获取bind2函数从第二个参数到最后一个参数
-    let args = Array.prototype.slice.call(arguments, 1);
-    return function () {
-        // 这个时候的arguments是指bind返回的函数传入的参数
-        var bindArgs = Array.prototype.slice.call(arguments);
-        self.apply(context, args.concat(bindArgs));
+    // 获取fakeBind函数从第二个参数到最后一个参数
+    let args = Array.prototype.slice.call(arguments, 1)
+    let result = function () {
+    	// 这个时候的arguments是指bind返回的函数传入的参数
+        let bindArgs = Array.prototype.slice.call(arguments);
+        // 1、当作为普通函数时，this 指向 window，self 指向绑定函数，此时结果为 false，当结果为 false 的时候，this 指向绑定的 context
+        // 2、当作为构造函数时，this 指向实例，self 指向绑定函数，此时结果为 true，（因为下面一句 `result.prototype = this.prototype;`），this 指向实例
+        self.apply(this instanceof self ? this : context, args.concat(bindArgs));
     }
+    // 修改返回函数的 prototype 为绑定函数的 prototype，实例就可以继承函数的原型中的值
+    result.prototype = Object.create(this.prototype);
+    return result;
 }
 
 
 
 
+let value = 2;
 let foo = {
     value: 1
 };
 function bar(name, age) {
+    this.habit = 'shopping';
     console.log(this.value);
     console.log(name);
     console.log(age);
-
 }
-let bindFoo = bar.fakeBind(foo, 'daisy');
-bindFoo('18');
-// 1
+bar.prototype.friend = 'kevin';
+
+
+let bindFoo = bar.bind(foo, 'daisy');
+let obj = new bindFoo('18');
+// undefined
 // daisy
 // 18
+console.log(obj.habit);
+console.log(obj.friend);
+// shopping
+// kevin
