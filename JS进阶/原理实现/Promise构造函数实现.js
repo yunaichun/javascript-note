bf502b4a3637bb783e684724f3c32c4972596331
余乃春 <yunaichun@hikvision.com.cn>
2018-11-16 18:01:13 +0800

    modify

--- JS进阶/原理实现/Promise构造函数实现.js
+++ JS进阶/原理实现/Promise构造函数实现.js
@@ -1,60 +1,74 @@
 /**
- * 参考地址：https://segmentfault.com/a/1190000009478377#articleHeader3
+ * 参考地址：https://github.com/xieranmaya/blog/issues/3
  */
 class PromiseNew {
-	// resolver为 function(resolve, reject){ ... }：主要是写resolve函数和reject函数
+	/*resolver为 function(resolve, reject){ ... }：主要是写resolve函数和reject函数*/
   constructor(executor) {
   	// 初始状态为pending
-    this.state = 'pending';
+    this.status = 'pending';
     // Promise的值
-    self.data = undefined;
+    this.data = undefined;
     // then成功回调注册函数
     this.onResolvedCallback = [];
     // then失败回调注册函数
     this.onRejectedCallback = [];
-    // 考虑到执行executor的过程中有可能出错，所以用try/catch块给包起来，并且在出错后以catch到的值reject掉这个Promise
+    /*考虑到执行executor的过程中有可能出错，所以用try/catch块给包起来，并且在出错后以catch到的值reject掉这个Promise*/
     try {
-      // 执行executor
+      /*executor是形参 -> 实例PromiseNew的时候会执行executor的参数 -> 执行内部resolve方法*/
       executor(this.resolve.bind(this), this.reject.bind(this)) 
     } catch(e) {
       this.reject(e)
     }
   }
 
-  // 更新state为：fullfilled，并且执行成功处理队列
+  /*更新status为：resolved，并且执行成功处理队列*/
   resolve(value) {
+    debugger; // 2
     let self = this;
-    if (self.state === 'pending') {
-      self.state = 'fullfilled';
+    if (self.status === 'pending') {
+      self.status = 'resolved';
       self.data = value;
-      // 一、加入延时机制：如果在then方法注册回调之前，resolve函数就执行了，怎么办？比如promise内部的函数是同步函数
-      setTimeout(function () {
-        self.onResolvedCallback.forEach((item, key, arr) => {
-          item.apply(null, value);
-          arr.shift();
-        });
-      }, 0);
+      // 这里面实际是收集了then函数中的回调
+      for(let i = 0; i < self.onResolvedCallback.length; i++) {
+        self.onResolvedCallback[i](value)
+      }
     }
   }
 
-  // 更新state为：rejected，并且执行失败处理队列
+  /*更新status为：rejected，并且执行失败处理队列*/
   reject(reason) {
     let self = this;
-    if (self.state === 'pending') {
-      self.state = 'rejected';
+    if (self.status === 'pending') {
+      self.status = 'rejected';
       self.data = reason;
-      // 一、加入延时机制：如果在then方法注册回调之前，resolve函数就执行了，怎么办？比如promise内部的函数是同步函数
-      setTimeout(function () {
-        self.onRejectedCallback.forEach((item, key, arr) => {
-          item.apply(null, reason);
-          arr.shift();
-        });
-      }, 0);
+      for(var i = 0; i < self.onRejectedCallback.length; i++) {
+        self.onRejectedCallback[i](reason)
+      }
     }
   }
 
-  // 同时注册成功和失败处理函数
+  /*同时注册成功和失败处理函数
+    一、then必须返回PromiseNew对象，而不是this
+      promise2 = promise1.then(function foo(value) {
+        return Promise.reject(3)
+      })
+      假如foo执行的话 -> 代表promise1是resolved
+      如果then返回this，即promise1，那么promise2不可能再执行Promise.reject(3)将自身状态改为rejected
+    
+    二、根据 promise1 的状态确定执行 onResolved 或者 onRejected
+      promise2 = promise1.then(function(value) {
+        return 4
+      }, function(reason) {
+        throw new Error('sth went wrong')
+      })
+      如果promise1被resolve了，此时promise1状态为resolved，promise2的将被4 resolve，
+      如果promise1被reject了，此时promise1状态为rejected，promise2将被new Error('sth went wrong') reject
+
+      所以，我们需要在then里面执行onResolved或者onRejected，并根据返回值来确定promise2的结果，
+      并且，如果onResolved/onRejected返回的是一个Promise，promise2将直接取这个Promise的结果。
+  */
   then(onResolved, onRejected) {
+    debugger; // 1
   	let self = this;
     let promise2;
 
@@ -62,32 +76,31 @@ class PromiseNew {
     onResolved = typeof onResolved === 'function' ? onResolved : function(v) {}
     onRejected = typeof onRejected === 'function' ? onRejected : function(r) {}
 
-    /*例子如下：
-      promise2 = promise1.then(function(value) {
-        return 4
-      }, function(reason) {
-        throw new Error('sth went wrong')
-      }) 
-    */
     if (self.status === 'resolved') {
+      // 返回新的PromiseNew实例
       return promise2 = new PromiseNew(function(resolve, reject) {
-        // 如果promise1的状态已经确定并且是resolved，我们调用onResolved
-        // 因为考虑到有可能throw，所以我们将其包在try/catch块里
+        // 因为考虑到onResolved函数有可能throw，所以将代码放在try/catch中
         try {
-          // 需要在then里面执行onResolved或者onRejected，并根据返回值(标准中记为x)来确定promise2的结果，
-          // 并且，如果onResolved/onRejected返回的是一个Promise，promise2将直接取这个Promise的结果
+          /*如果promise1的状态确定是resolved，则执行onResolved，onResolved执行的结果为x*/
           let x = onResolved(self.data);
-          // 如果onResolved的返回值是一个Promise对象，直接取它的结果做为promise2的结果
+          /*如果onResolved执行的结果是一个PromiseNew对象：
+            1、x的状态未知，取决于实际返回的PromiseNew对象
+            2、x调用then方法
+          */
           if (x instanceof PromiseNew) {
             x.then(resolve, reject);
           }
-          // 否则，以它的返回值做为promise2的结果
-          self.resolve(x);
+          /*如果onResolved执行的结果不是一个Promise对象：
+            1、onResolved正确时：promise2的值为x，状态为resolved
+            2、onResolved出错时：promise2的值为catch的err，状态为rejected
+          */
+          // 给promise2改状态以及赋值: self.status = 'resolved'; self.data = x;
+          resolve(x);
         } catch (e) {
-          // 如果出错，以捕获到的错误做为promise2的结果
-          self.reject(e);
+          // 给promise2改状态以及赋值: self.status = 'rejected'; self.data = e;
+          reject(e);
         }
-      })
+      });
     }
 
     if (self.status === 'rejected') {
@@ -100,44 +113,67 @@ class PromiseNew {
         } catch (e) {
           reject(e);
         }
-      })
+      });
     }
 
     if (self.status === 'pending') {
-      // 如果当前的Promise还处于pending状态，我们并不能确定调用onResolved还是onRejected，
-      // 只能等到Promise的状态确定后，才能确实如何处理。
-      // 所以我们需要把我们的**两种情况**的处理逻辑做为callback放入promise1(此处即this/self)的回调数组里
+      // 返回新的PromiseNew实例
       return promise2 = new PromiseNew(function(resolve, reject) {
+        /*如果promise1的状态还处于pending状态，我们并不能确定执行onResolved还是onRejected函数
+          所以先将其存入回调数组，待状态确定的时候再确定调用哪个函数
+        */
         self.onResolvedCallback.push(function(value) {
           try {
-            var x = onResolved(self.data);
+            let x = onResolved(self.data);
             if (x instanceof PromiseNew) {
               x.then(resolve, reject);
             }
           } catch (e) {
             reject(e);
           }
-        })
-
+        });
         self.onRejectedCallback.push(function(reason) {
           try {
-            var x = onRejected(self.data);
+            let x = onRejected(self.data);
             if (x instanceof PromiseNew) {
               x.then(resolve, reject);
             }
           } catch (e) {
             reject(e);
           }
-        })
-      })
+        });
+      });
     }
   }
 }
 
-// 测试
-new PromiseNew((resolve, reject) => {
-  resolve('hello world');
+/*实例new PromiseNew：
+  会执行executor函数 -> 执行内部resolve函数
+*/
+let promise1 = new PromiseNew((resolve, reject) => {
+  setTimeout(function() {
+    resolve('hello world');
+  }, 0);
+  // resolve('hello world');
   // reject('you are err');
-}).then(function(res) {
+});
+
+/*执行promise1.then函数 ->
+  判断promise1的状态 ->
+  根据promise1的状态确定是否立即执行onResolved函数（并不是立即执行onResolved）
+*/
+let promise2 = promise1.then(function(res) {
+  debugger; // 3
 	console.log(res);
+  return new PromiseNew((resolve, reject) => {
+    setTimeout(function() {
+      resolve('hello world1');
+    }, 0);
+    // resolve('hello world');
+    // reject('you are err');
+  });
+});
+
+let promise3 = promise2.then(function(res) {
+  console.log(res);
 });
