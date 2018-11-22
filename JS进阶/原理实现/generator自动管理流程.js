77b6ceb4ca3cfa7c7d23bf05c7999e4e619a410e
余乃春 <yunaichun@hikvision.com.cn>
2018-11-22 21:58:24 +0800

    Generator自动控制流程：将异步操作包装成 Thunk 函数，在回调函数里面交回执行权

--- JS进阶/原理实现/generator自动管理流程.js
+++ JS进阶/原理实现/generator自动管理流程.js
@@ -4,33 +4,13 @@
 */
 
 
-/*二、Thunk 函数的含义
-    编译器的"传名调用"实现，往往是将参数放到一个临时函数之中，再将这个临时函数传入函数体。这个临时函数就叫做 Thunk 函数。
 
-	function f(m){
-	  return m * 2;     
-	}
 
-	f(x + 5);
-
-	// 等同于
-
-	var thunk = function () {
-	  return x + 5;
-	};
-
-	function f(thunk){
-	  return thunk() * 2;
-	}
-
-	上面代码中，函数 f 的参数 x + 5 被一个函数替换了。凡是用到原参数的地方，对 Thunk 函数求值即可。
+/*二、Thunk 函数
+    1、编译器的"传名调用"实现，往往是将参数放到一个临时函数之中，再将这个临时函数传入函数体。这个临时函数就叫做 Thunk 函数。
+    2、在 JavaScript 语言中，Thunk 函数替换的不是表达式，而是多参数函数，将其替换成单参数的版本，且只接受回调函数作为参数！！！！！
 */
-
-
-/*三、JavaScript 语言的 Thunk 函数
-    在 JavaScript 语言中，Thunk 函数替换的不是表达式，而是多参数函数，将其替换成单参数的版本，且只接受回调函数作为参数。
-*/
-// ES5版本：参数有回调函数
+// ES5版本（Thunk 函数只接受回调函数作为参数）
 let ThunkES5 = function(fn) {
 	return function() {
 	    let args = Array.prototype.slice.call(arguments);
@@ -40,43 +20,137 @@ let ThunkES5 = function(fn) {
 	    }
 	};
 };
-// ES6版本：参数有回调函数
+// ES6版本（Thunk 函数只接受回调函数作为参数）
 let ThunkES6 = function(fn) {
   return function (...args) {
     return function (callback) {
-      return fn.call(this, ...args, callback);//call传入多参数
+      return fn.call(this, ...args, callback);
     };
   };
 };
-// 正常版本的readFile（多参数版本）
+// 正常调用 fs.readFile
 fs.readFile(fileName, callback);
-// 使用Thunk函数的readFile（多参数版本）
+// 使用 Thunk 函数的 readFile
 let readFileThunk = ThunkES5(fs.readFile);
 readFileThunk(fileA)(callback);
 
 
-/*四、Thunk 函数自动管理Generator流程*/
-function run(fn) {
-  let gen = fn();
 
-  function next(err, data) {
-    // gen.next()执行返回的是{ value: xxx, done: xxx }
-    let result = gen.next(data);
-    if (result.done) return;
-    // 用递归来自动完成这个过程
-    result.value(next);
-  }
 
-  // 初始执行一次，内部不断执行！！！！
-  next();
-}
-
-let gen = function* (){
-  let f1 = yield fs.readFile('fileA');
-  let f2 = yield fs.readFile('fileB');
-  // ...
-  let fn = yield fs.readFile('fileN');
+/*三、Thunkify 模块
+    Thunkify主要多了一个检查机制，变量 called 确保回调函数只运行一次。
+*/
+function thunkify(fn){
+    return function(){
+	    let ctx = this;
+	    let args = new Array(arguments.length);
+	    for(let i = 0; i < args.length; ++i) {
+	      args[i] = arguments[i];
+	    }
+	    return function(done) {
+	      let called;
+	      args.push(function() {
+	        if (called) return;
+	        called = true;
+	        done.apply(null, arguments); // arguments 是形参，sum 是实参
+	      });
+	      try {
+	        fn.apply(ctx, args);
+	      } catch (err) {
+	        done(err);
+	      }
+	    }
+    }
 };
+function f(a, b, callback) {
+    let sum = a + b;
+    callback(sum);
+    callback(sum);
+}
+let ft = thunkify(f);
+ft(1, 2)(console.log); // 3
+// 以上调用等价于：但是会调用两次
+f(1, 2, function(res) {
+	console.log.call(null, res);
+});
 
-// 内部不断调用调用next函数
+
+
+
+/*四、Generator注意点（https://blog.csdn.net/qq_30100043/article/details/53484350）
+
+  1、执行g.next(): 得到的是 yield 后面的值
+  2、执行g.next(value): 会将上一步 yield 后面的值 设置为 value
+  3、执行g.return(value): 得到结果为 { value, done:true } // 没有finally的情况
+
+  4、Generator 函数是遍历器生成函数，执行 Generator 函数会生成遍历器对象；
+     将 Generator 函数赋值给某对象的 Symbol.iterator 属性，则 该对象 具有 遍历器 接口；
+     可以被 for..of（可以自动遍历遍历器对象，且此时不再需要调用next方法）、Array.from、结构赋值、... 运算符遍历。
+     function* numbers() { yield 1; yield 2; return 3; yield 4; }
+     [...numbers()] // [1, 2]                       ->   遍历器接口：扩展运算符
+     Array.from(numbers()) // [1, 2]                ->   遍历器接口：Array.form 方法
+     let [x, y] = numbers(); // 1 2                 ->   遍历器接口：解构赋值
+     for (let n of numbers()) { console.log(n); }   ->   遍历器接口：for...of
+
+  5、function* gen() {}      let g = gen();
+     // g 遍历器对象 可以被 for...of 遍历，则可知 g 的 Symbol.iterator 的属性值等于 gen 遍历器生成函数
+     g[Symbol.iterator] === gen
+     g[Symbol.iterator]() === g
+
+  6、let jane = { first: 'Jane', last: 'Doe' }; // jane 对象此时不能被 for..of 遍历，因为 Symbol.iterator 的属性值不为 遍历器生成函数
+     jane[Symbol.iterator] = function* () {};   // jane 对象此时可以被 for..of 遍历，因为 Symbol.iterator 的属性值不为 遍历器生成函数
+*/
+
+
+
+
+/*五、Thunk 函数自动管理 Generator 流程
+      原理：回调函数，将异步操作包装成 Thunk 函数，在回调函数里面交回执行权。
+*/
+let fs = require('fs');
+let thunkify = require('thunkify');
+let readFile = thunkify(fs.readFile);
+let gen = function* (){
+    let f1 = yield readFile('fileA');
+    let f2 = yield readFile('fileB');
+    // ...
+    let fn = yield readFile('fileN');
+};
+/*法一：手动执行（通过 g.next() 的 value 属性获取到内部 thunk 函数）
+	上面代码中，yield 命令用于将程序的执行权移出 Generator 函数，那么就需要一种方法，将执行权再交还给 Generator 函数。
+	这种方法就是 Thunk 函数，因为它可以在回调函数里，将执行权交还给 Generator 函数！！！！！！！！！！！！！！！！！！
+*/
+let g = gen();
+let r1 = g.next();
+// 可以发现 Generator 函数的执行过程，其实是将同一个回调函数，反复传入 next 方法的 value 属性
+r1.value(function(err, data){ // 此处 value 是一个 thunk 函数！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+    if (err) throw err;
+    let r2 = g.next(data);
+    r2.value(function(err, data){
+        if (err) throw err;
+        g.next(data);
+    });
+});
+/*法二：自动执行（递归方法）
+	上面代码中，yield 命令用于将程序的执行权移出 Generator 函数，那么就需要一种方法，将执行权再交还给 Generator 函数。
+	这种方法就是 Thunk 函数，因为它可以在回调函数里，将执行权交还给 Generator 函数。
+*/
+function run(genCreator) {
+	let gen = genCreator();
+	// let r1 = gen.next();
+	// if (r1.done) { return; }
+	// r1.value(function(err, data) {
+	// 	  let r2 = g.next(data);
+	// 	  if (r2.done) { return; }
+	// 	  r2.value(function() {
+	// 		  ...
+	// 	  });
+	// });
+	function next(err, data) {
+		let result = gen.next(data); // data 是设置 Generator 内部 yield 的值
+		if (result.done) { return; }
+		result.value(next);
+	}
+	next();
+}
 run(gen);
