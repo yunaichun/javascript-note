c1c6bd9bbfe4dfeb3f00f76484c800b42cad6706
余乃春 <yunaichun@hikvision.com.cn>
2018-11-26 10:52:09 +0800

    async函数原理：返回自动执行的Generator函数

---
+++ JS进阶/原理实现/async函数原理.js
@@ -0,0 +1,28 @@
+async function fn(args) {
+	// ...
+}
+// 等价于
+function fn(args) {
+	return spawn(function* () {
+		// ...
+	});
+}
+
+// spawn 函数的实现:Generator自动执行器
+function spawn(genCreator) {
+	return new Promise(function(resolve, reject) {
+		let gen = genCreator();
+		function next(fuc) {
+			let result = fuc();
+			if (result.done) { return result.value; }
+			// 将值转换成 Promise 对象
+			Promise.resolve(result.value).then(function(value) {
+				next(function() {
+					return gen.next(value);
+				});
+			});
+		}
+		// 初始调用
+		next(function() { return gen.next(); });
+	});
+}
