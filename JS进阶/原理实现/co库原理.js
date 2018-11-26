586db3c7a85cedb7ff11450bbc0a62798cec6ddf
余乃春 <yunaichun@hikvision.com.cn>
2018-11-26 19:03:08 +0800

    co原理解析

--- JS进阶/原理实现/co库原理.js
+++ JS进阶/原理实现/co库原理.js
@@ -1,7 +1,12 @@
+let slice = Array.prototype.slice;
+
 /**参考：https://github.com/tj/co/blob/master/index.js
- * [co co库自动执行 Generator 函数]
- * @param  {[type]} gen [ Generator 函数]
- * @return {[type]}     [description]
+ * Execute the generator function or a generator
+ * and return a promise.
+ *
+ * @param {Function} Generator fn
+ * @return {Promise}
+ * @api public
  */
 function co(gen) {
     let ctx = this;
@@ -9,11 +14,16 @@ function co(gen) {
     return new Promise(function(resolve, reject) {
   	    // 执行遍历器函数，得到遍历器对象
   	    if (typeof gen === 'function') gen = gen.call(ctx);
-  	    // 执行遍历器函数，得到的不是遍历器对象，将 Promise 对象的状态改为 resolved 。
+  	    // 保证传入的值是遍历器函数
      	if (!gen || typeof gen.next !== 'function') return resolve(gen);
-	  	// 初始化入口函数，第一次调用
+
+	  	// Generator自动控制流程：对 next 函数做了一层包装，为了捕获异常
 	    onFulfilled();
-	    // 成功状态下的回调
+	    /**
+	     * @param {Mixed} res
+	     * @return {Promise}
+	     * @api private
+	     */
 	    function onFulfilled(res) {
 	        let ret;
 	        try {
@@ -25,6 +35,11 @@ function co(gen) {
 	        //开启调用链
 	        next(ret);
 	    }
+	    /**
+	     * @param {Error} err
+	     * @return {Promise}
+	     * @api private
+	     */
 	    function onRejected(err) {
 	        let ret;
 	        try {
@@ -35,7 +50,14 @@ function co(gen) {
 	        }
 	        next(ret);
 	    }
-     	// 递归调用【onFulefilled -> next：主要是为了能够捕捉抛出的错误】
+     	/**
+	     * Get the next value in the generator,
+	     * return a promise.
+	     *
+	     * @param {Object} ret
+	     * @return {Promise}
+	     * @api private
+	     */
 	    function next(ret) {
 			// 检查 Generator 函数是否执行到最后一步
 		    if (ret.done) return resolve(ret.value);
@@ -49,6 +71,134 @@ function co(gen) {
 		    }
 		}
 	});
+}
+
+/**
+ * Convert a `yield`ed value into a promise.
+ *
+ * @param {Mixed} obj
+ * @return {Promise}
+ * @api private
+ */
+function toPromise(obj) {
+  if (!obj) return obj;
+  if (isPromise(obj)) return obj; // 是Promise
+  if (isGeneratorFunction(obj) || isGenerator(obj)) return co.call(this, obj); // 是Generator
+  if ('function' == typeof obj) return thunkToPromise.call(this, obj); // 是 Thunk 函数
+  if (Array.isArray(obj)) return arrayToPromise.call(this, obj); // 是数组
+  if (isObject(obj)) return objectToPromise.call(this, obj); // 是对象
+  return obj;
+}
+
+/**
+ * Check if `obj` is a promise.
+ *
+ * @param {Object} obj
+ * @return {Boolean}
+ * @api private
+ */
+function isPromise(obj) {
+  return 'function' == typeof obj.then;
+}
+
+/**
+ * Check if `obj` is a generator function.（检验是否是遍历器函数）
+ *
+ * @param {Mixed} obj
+ * @return {Boolean}
+ * @api private
+ */
+function isGeneratorFunction(obj) {
+  var constructor = obj.constructor;
+  if (!constructor) return false;
+  // Generator 函数的 constructor 等于 GeneratorFunction ，普通函数的 constructor 等于 Function
+  if ('GeneratorFunction' === constructor.name || 'GeneratorFunction' === constructor.displayName) return true;
+  return isGenerator(constructor.prototype);
+}
+
+/**
+ * Check if `obj` is a generator.（检验是否是遍历器对象）
+ *
+ * @param {Mixed} obj
+ * @return {Boolean}
+ * @api private
+ */
+function isGenerator(obj) {
+  // 遍历器对象 GeneratorFunction {}，不同于普通的 Object 对象
+  return 'function' == typeof obj.next && 'function' == typeof obj.throw;
+}
+
+/**
+ * Check for plain object.
+ *
+ * @param {Mixed} val
+ * @return {Boolean}
+ * @api private
+ */
+
+function isObject(val) {
+  return Object == val.constructor;
+}
+
+/**
+ * Convert a thunk to a promise.
+ *
+ * @param {Function}
+ * @return {Promise}
+ * @api private
+ */
+function thunkToPromise(fn) {
+  var ctx = this;
+  return new Promise(function (resolve, reject) {
+    fn.call(ctx, function (err, res) {
+      if (err) return reject(err);
+      if (arguments.length > 2) res = slice.call(arguments, 1);
+      resolve(res);
+    });
+  });
+}
+
+/**
+ * Convert an array of "yieldables" to a promise.
+ * Uses `Promise.all()` internally.
+ *
+ * @param {Array} obj
+ * @return {Promise}
+ * @api private
+ */
+function arrayToPromise(obj) {
+  return Promise.all(obj.map(toPromise, this)); // 将数组的每一项依次转为 promise
+}
+
+/**
+ * Convert an object of "yieldables" to a promise.
+ * Uses `Promise.all()` internally.
+ *
+ * @param {Object} obj
+ * @return {Promise}
+ * @api private
+ */
+function objectToPromise(obj){
+  var results = new obj.constructor(); // 定义一个 Object
+  var keys = Object.keys(obj);
+  var promises = [];
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    var promise = toPromise.call(this, obj[key]); // 将对象的每一项依次转为 promise
+    if (promise && isPromise(promise)) defer(promise, key);
+    else results[key] = obj[key];
+  }
+  return Promise.all(promises).then(function () {
+    return results; // 执行所有 promise，返回每一项的 promise 结果【如果某一项不是promise，就返回其本身的值】
+  });
+
+  function defer(promise, key) {
+    // predefine the key in the result
+    results[key] = undefined;
+    promises.push(promise.then(function (res) {
+      results[key] = res;
+    }));
+  }
 }
 
 
@@ -73,8 +223,6 @@ co(function* () {
   ];
   console.log(res); 
 }).catch(onerror);
-
-
 // 测试二：并发操作（对象的写法）
 co(function* () {
   let res = yield {
