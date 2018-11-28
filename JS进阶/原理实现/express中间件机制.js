016602fd02a14351bfea244a82410ca3e96f67f7
余乃春 <yunaichun@hikvision.com.cn>
2018-11-28 16:53:42 +0800

    express中间件执行机制：中间件中执行下一个中间件

---
+++ JS进阶/原理实现/express中间件机制.js
@@ -0,0 +1,48 @@
+/**
+ * 仿照express实现中间件的功能
+ */
+let http = require('http');
+
+function express() {
+    let middleware = [];
+    let app = function(req, res) {
+        let i = 0;
+        function next() {
+            let task = middleware[i++];
+            if (!task) return;
+            // 执行 next 会调用下一个中间件
+            task(req, res, next);
+        }
+        next();
+    };
+    app.use = function(callback) {
+        middleware.push(callback);
+    }
+    return app;
+}
+
+
+// 测试
+let app = express();
+function middlewareA(req, res, next) {
+    console.log('middlewareA before next()');
+    next();
+    console.log('middlewareA after next()');
+}
+function middlewareB(req, res, next) {
+    console.log('middlewareB before next()');
+    next();
+    console.log('middlewareB after next()');
+}
+function middlewareC(req, res, next) {
+    console.log('middlewareC before next()');
+    next();
+    console.log('middlewareC after next()');
+}
+app.use(middlewareA);
+app.use(middlewareB);
+app.use(middlewareC);
+
+http.createServer(app).listen('3000', function () {
+    console.log('listening 3000....');
+});
