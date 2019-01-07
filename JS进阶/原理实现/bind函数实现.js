8fb118440dd5a61e300944962a1d85441f1b76b2
余乃春 <yunaichun@hikvision.com.cn>
2019-01-07 10:19:14 +0800

    fakeBind函数再次理解

--- JS进阶/原理实现/bind函数实现.js
+++ JS进阶/原理实现/bind函数实现.js
@@ -3,29 +3,24 @@
  * @param  {Object}       context       [context对象]
  * @return {[Function]}                 [返回函数]
  */
-Function.prototype.fakeBind = function (context) {
+Function.prototype.fakeBind = function(context) {
     if (typeof this !== 'function') {
 	  throw new Error('when use bind, it must be a function');
 	}
     let self = this;
     // 获取fakeBind函数从第二个参数到最后一个参数
     let args = Array.prototype.slice.call(arguments, 1)
-    let resultFuc = function () {
+    let resultFuc = function() {
     	// 这个时候的arguments是指bind返回的函数传入的参数
         let bindArgs = Array.prototype.slice.call(arguments);
-        // 1、当作为普通函数时，this 指向 window，self 指向绑定函数，此时结果为 false，当结果为 false 的时候，this 指向绑定的 context。（content为null或undefined均指向window）
-        // 2、当作为构造函数时，this 指向实例，self 指向绑定函数，此时结果为 true，（因为下面一句 `result.prototype = this.prototype;`），this 指向实例
+        // 1、当作为普通函数时，this 指向 context
+        // 2、当作为构造函数时，this 指向 实例
         self.apply(this instanceof self ? this : context, args.concat(bindArgs));
     }
-    // 修改返回函数的 prototype 为绑定函数的 prototype，实例就可以继承函数的原型中的值
+    // 1、可以判断是否是 new 的操作  2、返回函数只会继承 self 原型上的属性
     resultFuc.prototype = Object.create(self.prototype);
     return resultFuc;
 }
-
-
-
-
-let value = 2;
 let foo = {
     value: 1
 };
@@ -36,17 +31,28 @@ function bar(name, age) {
     console.log(age);
 }
 bar.prototype.friend = 'kevin';
+/*一、测试普通函数*/
+let bindFoo1 = bar.fakeBind(foo, 'daisy');
+let obj1 = bindFoo1('18'); // 1 + daisy + 18
+console.log(obj1.habit);// TypeError
+console.log(obj1.friend);// TypeError
+/*二、测试构造函数*/
+let bindFoo2 = bar.fakeBind(foo, 'daisy');
+let obj2 = new bindFoo2('18'); // undefined + daisy + 18
+/*注意：假如为 this instanceof self 时 为 self
+  则此时 obj2 会执行两个操作：
+  1、执行 new   操作：继承 bar.prototype 的属性
+  2、执行 apply 操作：bar.apply(bar, ['daisy', '18'])
+*/
+/*但是：假如为 this instanceof self 时 为 this
+  则此时 obj2 会执行两个操作：
+  1、执行 new   操作：继承 bar.prototype 的属性
+  2、执行 apply 操作：bar.apply(obj2, ['daisy', '18'])，此时实例还会继承 bar 构造函数上的属性
+*/
+console.log(obj2.habit);// shopping
+console.log(obj2.friend);// kevin
 
 
-let bindFoo = bar.bind(foo, 'daisy');
-let obj = new bindFoo('18');
-// undefined
-// daisy
-// 18
-console.log(obj.habit);
-console.log(obj.friend);
-// shopping
-// kevin
 
 
 
