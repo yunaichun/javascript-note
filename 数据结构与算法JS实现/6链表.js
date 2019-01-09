28bebaa194a7cbdb1e666bda2da6a6ab1b098afc
余乃春 <yunaichun@hikvision.com.cn>
2019-01-09 11:41:03 +0800

    整理代码

--- 数据结构与算法JS实现/6链表.js
+++ 数据结构与算法JS实现/6链表.js
@@ -1,67 +1,72 @@
-//定义node类，目的是保存节点
-function Node(data,next){
-       this.data=data;//此节点对应的数据
-	   this.next=next;//指向下一个节点的连接。每次创建默认都为null
+/**
+* [Node 链表节点：存储数据和下一个节点]
+*/
+function Node(data, next) {
+       this.data = data; // 此节点对应的数据
+       this.next = next; // 指向下一个节点的连接 ，每次创建默认都为null，初始也为null
 }
 
-//定义链表操作的方法
-function Llist(){
-	this.head=new Node("head",null);//保存链表的头节点（可用this.head.data取得此节点数据，this.head.next取得下一个节点的链接
+// 定义链表操作的方法
+function Llist() {
+    this.head = new Node('head', null); // 保存链表的头节点
 }
-//在已知节点currentData后插入data
-Llist.prototype.insert=function(data,currentNode){
-     var newNode=new Node(data,null);//每次插入默认下一个节点都为空
-	 var currentNode=this.find(currentNode);//已知节点item，要将新节点插入到已知节点后面
-	 newNode.next=currentNode.next;//第一步：新节点指向当前节点的下一个节点
-	 currentNode.next=newNode;//第二步：当前节点指向新节点
+/**
+* [find 查询某数据所属的节点]
+* @param  {[type]} data [待查询的数据]
+* @return {[type]}      [返回此数据对应的节点]
+*/
+Llist.prototype.find = function(data) {
+     var currNode = this.head;
+     while (currNode.data !== data) {
+         currNode = currNode.next;
+     }
+     return currNode;
 }
-//在已知节点后面插入元素，find要找到已知节点item原来指向的节点
-Llist.prototype.find=function(data){
-     var currNode=this.head;//从根节点开始循环
-	 //循环遍历链表所有节点(while指的是循环啊啊啊啊)
-	 while(currNode.data!=data){//如果当前节点的属性：数据  与已知节点不同，
-		 currNode=currNode.next;//则将节点的指向移动到下一个节点
-	 }
-	 return currNode;//相同的话，则说明找到成功，返回已知节点
+/**
+* [insert 将新数据节点插入到指定数据节点的后面]
+* @param  {[type]} newData         [待插入的新数据]
+* @param  {[type]} prevData        [新数据节点之前的节点对应的数据]
+* @return {[type]}                 [description]
+*/
+Llist.prototype.insert = function(newData, prevData) {
+     var newNode = new Node(newData, null);
+     var prevNode = this.find(prevData);
+     // 两者顺序不能颠倒了
+     newNode.next = prevNode.next;
+     prevNode.next = newNode;
 }
-
-//删除节点首先要找到要删除节点前面的节点
-Llist.prototype.findPrevious=function(data){
-     var currNode=this.head;//从根节点开始循环
-	 //循环遍历所有节点，知道某一个节点的下一个节点指向传进来的节点为止
-	 while(!(currNode.next==null)&&(currNode.next.data!=data)){
-		 currNode=currNode.next;
-	 }
-	 return currNode;//返回传进来的item节点的前一个节点
+/**
+* [findPrevious 查询某数据所属的节点对应的前一个节点]
+* @param  {[type]} data [待查询的数据]
+* @return {[type]}      [返回此数据对应的节点的前一个节点]
+*/
+Llist.prototype.findPrevious = function(data) {
+     var prevNode = this.head;
+     while ((prevNode.next !== null) && (prevNode.next.data !== data)) {
+         prevNode = prevNode.next;
+     }
+     return prevNode;
 }
-//删除节点item
-Llist.prototype.remove=function(item){
-	var prevNode=this.findPrevious(item);
-	if(!(prevNode.next==null)){
-		prevNode.next=prevNode.next.next;//一步:将要删除的节点的前一个节点直接指向要删除节点的后一个节点
-	}
+/**
+* [remove description]
+* @param  {[type]} data [待插入的新数据]
+* @return {[type]}      [description]
+*/
+Llist.prototype.remove = function(data) {
+    var prevNode = this.findPrevious(data);
+    if (prevNode.next !== null) {
+        prevNode.next = prevNode.next.next;
+    }
 }
-//显示链表数据
-Llist.prototype.display=function(){
-	var currNode=this.head;//从链表的头开始 
-	//循环遍历链表，当前节点的next属性为null时结束循环
-	while(!(currNode.next==null)){
-      console.log(currNode.next.data);//链表的头是head，链表的尾是null
-	  currNode=currNode.next;
-	}
+Llist.prototype.display = function() {
+    var currNode = this.head;
+    while (currNode.next !== null) {
+      console.log(currNode.next.data);
+      currNode = currNode.next;
+    }
 }
-
-
-
-
-var num=new Llist();
-num.insert("11","head");
-num.insert("22","11");
-num.insert("33","22");
-num.insert("44","33");
-num.display();
-num.remove("44");
-num.display();
-
-
-//此外还有双向链表和循环链表就不叙述了。
+// 测试
+var list = new Llist();
+list.insert(1, 'head');
+list.insert(2, 1);
+list.insert(3, 1);
