c6974a43db28570f4820c63d8ff9b7efc2f0585c
余乃春 <yunaichun@hikvision.com.cn>
2019-01-09 22:28:17 +0800

    修改链表插入方法

--- 数据结构与算法JS实现/6链表.js
+++ 数据结构与算法JS实现/6链表.js
@@ -8,7 +8,7 @@ function Node(data, next) {
 
 // 定义链表操作的方法
 function Llist() {
-    this.head = new Node('head', null); // 保存链表的头节点
+    this.head = new Node(null, null); // 保存链表的头节点
 }
 /**
 * [insertAfter 将待插入的数据插入到目标节点数据的后面]
@@ -19,28 +19,40 @@ function Llist() {
 Llist.prototype.insertAfter = function(sourceData, targetData) {
     var sourceNode = new Node(sourceData, null);
     var targetNode = this.head;
-    while (targetNode !== null && targetNode.data !== targetData) {
-        targetNode = targetNode.next;
+    if (targetNode.data === null) {
+        this.head.data = sourceNode.data;
+    } else {
+        while (true) {
+            if (targetNode.data !== targetData) {
+                targetNode = targetNode.next;
+            } else {
+                break;
+            }
+        }
+        // 两者顺序不能颠倒了
+        sourceNode.next = targetNode.next;
+        targetNode.next = sourceNode;
     }
-    // 两者顺序不能颠倒了
-    sourceNode.next = targetNode.next;
-    targetNode.next = sourceNode;
 }
 /**
 * [remove 移除链表上指定数据的节点]
-* @param  {[type]} data [待移除的节点的数据]
-* @return {[type]}      [description]
+* @param  {[type]} targetData [待移除的节点的数据]
+* @return {[type]}            [description]
 */
 Llist.prototype.remove = function(targetData) {
     var targetNode = this.head;
-    while (targetNode !== null && targetNode.next.data !== targetData) {
-        targetNode = targetNode.next;
+    while (true) {
+        if (targetNode.next.data !== targetData) {
+            targetNode = targetNode.next;
+        } else {
+            break;
+        }
     }
     targetNode.next = targetNode.next.next;
 }
 // 测试
 var list = new Llist();
-list.insertAfter(1, 'head');
+list.insertAfter(1);
 list.insertAfter(2, 1);
 list.insertAfter(3, 1);
 list.remove(3);
@@ -48,15 +60,12 @@ list.remove(3);
 /*  数据结构：
     Llist {
         head: Node {
-            data: 'head',
+            data: 1,
             next: Node {
-                data: 1,
+                data: 3,
                 next: Node {
-                    data: 3,
-                    next: Node {
-                        data: 2,
-                        next: null
-                    }
+                    data: 2,
+                    next: null
                 }
             }
         }
