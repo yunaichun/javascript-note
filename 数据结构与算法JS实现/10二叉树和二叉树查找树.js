0a1408f102e44d4fe497e80b421e81f59e336d35
余乃春 <yunaichun@hikvision.com.cn>
2019-01-09 22:07:02 +0800

    二叉树插入方法modify

--- 数据结构与算法JS实现/10二叉树和二叉树查找树.js
+++ 数据结构与算法JS实现/10二叉树和二叉树查找树.js
@@ -1,93 +1,128 @@
-//定义节点
-function Node(data,left,right){
-	this.data=data;
-	this.left=left;//指向左子节点的连接。每次创建默认都为null。初始值也为null
-	this.right=right;//指向右子节点的连接。每次创建默认都为null。初始值也为null
-	//显此节点的数据
-	this.show=function(){
-		return this.data;
-	};
+/**
+* [Node 二叉树节点：存储数据和左右节点]
+*/
+function Node(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+    this.show = function() {
+        return this.data
+    };
 }
 
 //定义二叉树
-function BST(){
-	this.root=new Node(null,null,null);//默认根节点为空
+function BST() {
+    this.head = new Node(null, null, null);
+}
+/**
+* [insert 将新数据节点插入到二叉树指定位置]
+* @param  {[type]} newData         [待插入的新数据]
+* @param  {[type]} prevData        [新数据节点之前的节点对应的数据]
+* @return {[type]}                 [description]
+*/
+BST.prototype.insert = function(sourceData) {
+    var sourceNode = new Node(sourceData, null, null);
+    var targetNode = this.head;
+    // 存入头节点的值：相当于基准值
+    if (targetNode.data === null) {
+    	targetNode.data = sourceNode.data;
+    } else {
+	    while (true) {
+	        if (sourceData < targetNode.data) {
+	        	if (targetNode.left === null) {
+	        		targetNode.left = sourceNode;
+	        		break;
+	        	} else {
+	        		targetNode = targetNode.left;
+	        	}
+	        } else {
+	            if (targetNode.right === null) {
+	            	targetNode.right = sourceNode;
+	        		break;
+	        	} else {
+	        		targetNode = targetNode.right;
+	        	}
+	        }
+	    }
+    }
+}
+/**
+* [inOrder 中序：左 -> 根 -> 右]
+* @param  {[type]} node [二叉树某一节点：可以传入跟节点this.root]
+* @return {[type]}      [description]
+*/
+BST.prototype.inOrder = function(node) {
+    if (node !== null) {
+        inOrder(node.left);
+        console.log(node.show() + ' ');
+        inOrder(node.right);
+    }
+}
+/**
+* [preOrder 先序：根 -> 左 -> 右]
+* @param  {[type]} node [二叉树某一节点：可以传入跟节点this.root]
+* @return {[type]}      [description]
+*/
+BST.prototype.preOrder = function(node) {
+    if (node !== null) {
+        console.log(node.show() + ' ');
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+}
+/**
+* [postOrder 后序：左 -> 右 -> 根]
+* @param  {[type]} node [二叉树某一节点：可以传入跟节点this.root]
+* @return {[type]}      [description]
+*/
+BST.prototype.postOrder = function(node) {
+    if (node !== null){
+        postOrder(node.left);
+        postOrder(node.right);
+        console.log(node.show() + ' ');
+    }
+}
+/**
+* [getMin 获取最小值]
+* @return {[type]} [description]
+*/
+BST.prototype.getMin = function() {
+    var currNode = this.root;
+    while (currNode.left !== null) {
+        currNode = cerrent.left;
+    }
+    return currNode.data;
+}
+/**
+* [getMax 获取最大值]
+* @return {[type]} [description]
+*/
+BST.prototype.getMax = function() {
+    var currNode = this.root;
+    while (currNode.right !== null) {
+        currNode = currNode.right;
+    }
+    return currNode.data;
 }
 
-//向二叉树中添加节点和数据
-BST.prototype.insert=function(data){
-	var newNode =new Node(data,null,null);//每次插入默认左、右子节点都为空
-	var current=this.root;//默认从根节点开始插入
-	if(current==null){//第一次插入数据
-		current.data=newNode;
-	}else{//第二次及以上插入数据、
-		while(current!=null){
-			if(data<current.data){//数据在左边
-				current=current.left;
-			}else{//数据在右边
-				current=current.right;
-			}
-		}
-		current.data=newNode;
-	}
-}
-
-//中序排序（先访问左节点，再根节点，最后右子节点）
-//inOrder(nums.root);调用的时候从根节点开始，传入根节点
-BST.prototype.inOrder=function(node){
-	if(!(node.data==null)){
-		inOrder(node.left);//左
-		console.log(node.show()+" ");//根
-		inOrder(node.right);//右
-	}
-}
-
-//先序排序（先访问根节点，再访问左子节点，最后访问右子点）
-BST.prototype.preOrder=function(node){
-	if(!(node.data=null)){
-		console.log(node.show()+" ");//根
-		preOrder(node.left);//左
-		preOrder(node.right);//右
-	}
-}
-
-//后序排序（从叶子节点开始，访问左子节点，右子节点，最后访问根节点）
-BST.prototype.postOrder=function(node){
-	if(!(node.data==null)){
-		postOrder(node.left);//左
-		postOrder(node.right);//右
-		console.log(node.show()+" ");//根
-	}
-}
-
-//查找最小值
-BST.prototype.getMin=function(){
-	var current=this.root;
-	while(!(current.left==null)){
-		current=cerrent.left;
-	}
-	return current.data;
-}
-
-//查找最大值
-BST.prototype.getMax=function(){
-	var current=this.root;
-	while(!(current.right==null)){
-		current=current.right;
-	}
-	return current.data;
-}
-
-var sz=new BST();
-sz.insert("11");
-sz.insert("22");
-sz.insert("33");
-sz.insert("44");
-console.log("二叉树中序（从小到大）遍历查询结果：");
-sz.inOrder(sz.root);
-//console.log("二叉树最大值：")；
-console.log("二叉树中最大值是："+sz.getMax());
-//console.log("二叉树最小值：")；
-console.log("二叉树中最小值是："+sz.getMin());
-console.log("二叉树中55值是："+sz.find("55"));
-console.log("二叉树中44值是："+sz.find("44"));
+var bst = new BST();
+bst.insert(11);
+bst.insert(10);
+bst.insert(12);
+/*  数据结构：
+    BST {
+        head: Node {
+            data: 11,
+            left: Node {
+                data: 10,
+                left: null,
+                right: null
+            },
+            right: Node {
+                data: 12,
+                left: null,
+                right: null
+            }
+        }
+    }
+*/
