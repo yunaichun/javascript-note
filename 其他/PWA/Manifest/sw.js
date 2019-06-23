d96c29cc60135389a561621206a793dc2b91e167
yunaichun <naichun.yu@cootek.cn>
2019-06-24 00:10:18 +0800

    what PWA is

--- 其他/PWA/Manifest/sw.js
+++ 其他/PWA/Manifest/sw.js
@@ -1,6 +1,13 @@
-/*API地址参考：https://developers.google.com/web/fundamentals/codelabs/your-first-pwapp/*/
+/*API地址参考：https://developers.google.com/web/fundamentals/codelabs/your-first-pwapp/
+  参考地址：https://juejin.im/post/5a9e8ad5f265da23a40456d4
+*/
 
 var cacheName = 'helloWorld'
+var filesToCache = [
+  '/',  // 这个一定要包含整个目录，不然无法离线浏览
+  './PWA.png',
+  './index.html',
+];
 
 /* 注册serviceWorker服务工作线程后，用户首次访问页面时将会触发install安装事件。
    处理：在此事件处理程序内，我们将缓存应用所需的全部资源数据
@@ -14,11 +21,7 @@ self.addEventListener('install', event => {
     .then(cache => {
       console.log('[ServiceWorker] Caching app shell');
       // cache.addAll() 具有原子性，如果任何一个文件失败，整个缓存步骤也将失败！
-      cache.addAll([
-        '/',  // 这个一定要包含整个目录，不然无法离线浏览
-        './PWA.png',
-        './index.html',
-      ])
+      cache.addAll(filesToCache)
     }).then(() => self.skipWaiting())
   )
 })
@@ -47,13 +50,23 @@ self.addEventListener('activate', event => {
 /* 决定想要如何处理请求，并可提供我们自己的已缓存响应
    处理：拦截 Progressive Web App 发出的请求并在服务工作线程内对它们进行处理的能力
 */
-self.addEventListener('fetch', event => {
-  console.log('[ServiceWorker] Fetch', event.request.url);
-  event.respondWith(
-    // caches.match() 会由内而外对触发fetch事件的网络请求进行评估，并检查以确认它是否位于缓存内
-    caches.match(event.request)
-    .then(response => {
-      return response || fetch(event.request);
-    })
-  )
-})
+self.addEventListener('fetch', (e) => {
+    // 判断地址是不是需要实时去请求，是就继续发送请求
+    if (e.request.url.indexOf('/api/400/200') > -1) {
+        e.respondWith(
+            caches.open(imgCacheName).then(function(cache){
+                 return fetch(e.request).then(function (response){
+                    cache.put(e.request.url, response.clone()); // 每请求一次缓存更新一次新加载的图片
+                    return response;
+                });
+            })
+        );
+    } else {
+        e.respondWith(
+            // 匹配到缓存资源，就从缓存中返回数据。caches.match() 会由内而外对触发fetch事件的网络请求进行评估，并检查以确认它是否位于缓存内
+            caches.match(e.request).then(function (response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
+});
