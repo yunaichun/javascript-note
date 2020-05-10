a49f8f6f73163fd76c6d53308e4e984dc20c3257
yunaichun <yunaichun@qingting.fm>
2020-05-11 01:57:00 +0800

    链表js和py实现

--- 数据结构与算法/极客时间/链表/实现/链表.js
+++ 数据结构与算法/极客时间/链表/实现/链表.js
@@ -1,6 +1,44 @@
-class ListNode {
-    constructor(val = 0, next = null) {
+class Node {
+    constructor(val = null, next = null) {
         this.val = val
         this.next = next
     }
 }
+
+class ListNode {
+    constructor() {
+        this.head = new Node();
+    }
+
+    insertAfter(sourceNode, targetNode) {
+        if (this.head.val === null && this.head.next === null) {
+            this.head = sourceNode
+        } else {
+            sourceNode.next = targetNode.next
+            targetNode.next = sourceNode
+        }
+        return this.head
+    }
+    
+    removeNode(node) {
+        let first = this.head;
+        while(first.next) {
+            if (first.next === node) {
+                first.next = first.next.next
+                break
+            }
+            first = first.next
+        }
+        return this.head
+    }
+}
+
+let a = new Node(1)
+let b = new Node(2)
+let c = new Node(3)
+let list = new ListNode(a)
+list.insertAfter(a)
+list.insertAfter(b, a)
+list.insertAfter(c, b)
+list.removeNode(b)
+console.log(list)
--- 数据结构与算法/极客时间/链表/实现/链表.py
+++ 数据结构与算法/极客时间/链表/实现/链表.py
@@ -1,4 +1,36 @@
-class ListNode:
-    def __init__(self, val = 0, next = None):
+class Node:
+    def __init__(self, val = None, next = None):
         self.val = val
         self.next = next
+
+class ListNode:
+    def __init__(self):
+        self.head = Node();
+
+    def insertAfter(self,sourceNode, targetNode = None):
+        if not self.head.val and not self.head.next:
+            self.head = sourceNode
+        else:
+            sourceNode.next = targetNode.next
+            targetNode.next = sourceNode
+        return self.head
+
+    def removeNode(self, node):
+        first = self.head
+        while first.next:
+            if first.next is node:
+                first.next = first.next.next
+                break
+            first = first.next
+        return self.head
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+list = ListNode()
+list.insertAfter(a)
+list.insertAfter(b, a)
+list.insertAfter(c, b)
+list.removeNode(b)
+print(list)
+print(__name__)
