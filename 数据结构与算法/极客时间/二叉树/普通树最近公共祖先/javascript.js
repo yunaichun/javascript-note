92d543a0a04a4f777ae7418477eca9712ec1c285
yunaichun <yunaichun@qingting.fm>
2020-05-28 17:29:12 +0800

    普通二叉树最近公共祖先

---
+++ 数据结构与算法/极客时间/二叉树/普通树最近公共祖先/javascript.js
@@ -0,0 +1,29 @@
+// == leetcode: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+class Solution {
+    constructor(props) {
+        super(props)
+    }
+    lowestCommonAncestor(root, p, q) {
+        if (root === null) {
+           return null;
+        } else {
+            if (root === p || root === q) {
+                return root;
+            } else {
+                // == root 即不为 q 也不为 p
+                let left = this.lowestCommonAncestor(root.left, p, q);
+                let right = this.lowestCommonAncestor(root.right, p, q);
+                if (left !== null && right !== null) {
+                    return root;
+                }
+                if (left === null && right !== null) {
+                    return right;
+                }
+                if (left !== null && right === null) {
+                    return left;
+                }
+            }
+        }
+    }
+}
+
---
+++ 数据结构与算法/极客时间/二叉树/普通树最近公共祖先/python.py
@@ -0,0 +1,20 @@
+class Solution:
+    def __init__(self):
+        super().__init__()
+
+    def lowestCommonAncestor(self, root, p, q):
+        if root == None:
+           return None
+        else:
+            if root == p or root == q:
+                return root
+            else:
+                left = self.lowestCommonAncestor(root.left, p, q)
+                right = self.lowestCommonAncestor(root.right, p, q)
+                if left != None and right != None:
+                    return root
+                if left == None and right != None:
+                    return right
+                if left != null and right == null:
+                    return left
+        
