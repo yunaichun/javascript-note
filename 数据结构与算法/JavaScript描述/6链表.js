7312e0388fa5514f64d347512ccbb875240514cf
yunaichun <yunaichun@qingting.fm>
2020-05-28 11:06:26 +0800

    重写链表实现

--- 数据结构与算法/JavaScript描述/6链表.js
+++ 数据结构与算法/JavaScript描述/6链表.js
@@ -1,73 +1,44 @@
-/**
-* [Node 链表节点：存储数据和下一个节点]
-*/
-function Node(data, next) {
-    this.data = data; // 此节点对应的数据
-    this.next = next; // 指向下一个节点的连接 ，每次创建默认都为null，初始也为null
-}
-
-// 定义链表操作的方法
-function Llist() {
-    this.head = new Node(null, null); // 保存链表的头节点
-}
-/**
-* [insertAfter 将待插入的数据插入到目标节点数据的后面]
-* @param  {[type]} sourceData     [待插入的数据]
-* @param  {[type]} targetData     [目标节点数据]
-* @return {[type]}                [description]
-*/
-Llist.prototype.insertAfter = function(sourceData, targetData) {
-    var sourceNode = new Node(sourceData, null);
-    var targetNode = this.head;
-    if (targetNode.data === null) {
-        this.head.data = sourceNode.data;
-    } else {
-        while (true) {
-            if (targetNode.data !== targetData) {
-                targetNode = targetNode.next;
-            } else {
-                break;
-            }
-        }
-        // 两者顺序不能颠倒了
-        sourceNode.next = targetNode.next;
-        targetNode.next = sourceNode;
+class Node {
+    constructor(val = null, next = null) {
+        this.val = val
+        this.next = next
     }
 }
-/**
-* [remove 移除链表上指定数据的节点]
-* @param  {[type]} targetData [待移除的节点的数据]
-* @return {[type]}            [description]
-*/
-Llist.prototype.remove = function(targetData) {
-    var targetNode = this.head;
-    while (true) {
-        if (targetNode.next.data !== targetData) {
-            targetNode = targetNode.next;
+
+class ListNode {
+    constructor() {
+        this.head = new Node();
+    }
+
+    insertAfter(sourceNode, targetNode) {
+        if (this.head.val === null && this.head.next === null) {
+            this.head = sourceNode
         } else {
-            break;
+            sourceNode.next = targetNode.next
+            targetNode.next = sourceNode
         }
+        return this.head
     }
-    targetNode.next = targetNode.next.next;
-}
-// 测试
-var list = new Llist();
-list.insertAfter(1);
-list.insertAfter(2, 1);
-list.insertAfter(3, 1);
-list.remove(3);
-
-/*  数据结构：
-    Llist {
-        head: Node {
-            data: 1,
-            next: Node {
-                data: 3,
-                next: Node {
-                    data: 2,
-                    next: null
-                }
+    
+    removeNode(node) {
+        let first = this.head;
+        while(first.next) {
+            if (first.next === node) {
+                first.next = first.next.next
+                break
             }
+            first = first.next
         }
+        return this.head
     }
-*/
+}
+
+let a = new Node(1)
+let b = new Node(2)
+let c = new Node(3)
+let list = new ListNode(a)
+list.insertAfter(a)
+list.insertAfter(b, a)
+list.insertAfter(c, b)
+list.removeNode(b)
+console.log(list)
