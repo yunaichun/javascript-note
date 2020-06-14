b7e24c83f9b7b16981d0d35e12df8dc57f2c4927
yunaichun <yunaichun@qingting.fm>
2020-06-15 02:13:46 +0800

    字梯2求结果

--- 数据结构与算法/极客时间/二叉树/字梯/javascript.js
+++ 数据结构与算法/极客时间/二叉树/字梯/javascript.js
@@ -1,4 +1,5 @@
 // == leetcode: https://leetcode.com/problems/word-ladder/
+// == leetcode: https://leetcode.com/problems/word-ladder-ii/
 class Solution {
     constructor() {
         this.letters = 'abcdefghijklmnopqrstuvwxyz';
@@ -13,16 +14,32 @@ class Solution {
         let step = this._bfs(beginWord, endWord, wordList);
         return step;
     }
+    /**
+     * @param {string} beginWord
+     * @param {string} endWord
+     * @param {string[]} wordList
+     * @return {string[][]}
+     */
+    findLadders(beginWord, endWord, wordList) {
+
+    }
     // == 广度优先 o(n)
     _bfs(beginWord, endWord, wordList) {
         const dict = new Set(wordList);
         let step = 1;
+        let result = [beginWord];
         let queue = [beginWord];
         while (queue.length) {
             let levelSize = queue.length;
+            let currentLevel = []
             // == 遍历当前 level 所有单词
             for (let i = 0; i < levelSize; i++) {
-                if (queue[i] === endWord) return step;
+                if (queue[i] === endWord) {
+                    // == 在这里只需要看倒数第二项所有 word 是否可以转换为最后一个单词
+                    // == 如果可以的话就可以的有倒数第二项的长度种可能
+                    console.log(result);
+                    return step;
+                }
                 // == 对当前 level 上每个单词的每一位做变换
                 for (let j = 0; j < queue[i].length; j++) {
                     // == 每一位都可以变换为 26 个英文字母的一项
@@ -31,6 +48,7 @@ class Solution {
                         // == 在下一步如果还能够变换到同一层级的单词，相当于会再多走一步
                         if (dict.has(newWord)) {
                             queue.push(newWord);
+                            currentLevel.push(newWord);
                             dict.delete(newWord);
                         }
                     }
@@ -38,6 +56,7 @@ class Solution {
             }
             queue = queue.slice(levelSize);
             step++;
+            result.push(currentLevel)
         }
         return 0;
     }
