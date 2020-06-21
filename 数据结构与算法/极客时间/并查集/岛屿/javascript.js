445629ec3a3b1f411abfaff3082da86d35a2472a
yunaichun <yunaichun@qingting.fm>
2020-06-21 16:13:05 +0800

    染色递归求岛屿数量

--- 数据结构与算法/极客时间/并查集/岛屿/javascript.js
+++ 数据结构与算法/极客时间/并查集/岛屿/javascript.js
@@ -2,12 +2,97 @@
 // == leetcode: https://leetcode.com/problems/friend-circles/
 class Solution {
     constructor() {
+        this.queue = [];
+        this.visited = new Set();
+        // == 横坐标：左、右、上、下
+        this.dx = [-1, 1, 0, 0];
+        // == 纵坐标：左、右、上、下
+        this.dy = [0, 0, -1, 1];
     }
     /**
      * @param {number[][]} grid
      * @return {number}
      */
     numIslands(grid) {
-
+        for (let i = 0, row = grid.length; i < row; i++) {
+            for (let j = 0, column = grid[i].length; j < column; j++) {
+                this._dfs(grid, i, j);
+            }
+        }
+        console.log(111111, grid);
+        let sum = this._get_result(grid);
+        return sum;
+    }
+    // == 深度优先 o(n)  -  染色
+    _dfs(grid, row, column) {
+        if (!this._is_valid(grid, row, column)) {
+            return;
+        }
+        this.visited.add({row, column});
+        for (let i = 0; i < 4; i++) {
+            let newRow = row + this.dx[i];
+            let newColumn = column + this.dy[i];
+            if (this._is_valid(grid, newRow, newColumn)) {
+                grid[row][column] = 0;
+                this._dfs(grid, newRow, newColumn);
+            }
+        }
+    }
+    // == 广度优先 o(n)  -  染色
+    _bfs(grid, row, column) { n
+        if (!this._is_valid(grid, row, column)) {
+            return;
+        }
+        this.visited.add({row, column});
+        this.queue.push({row, column});
+        while(this.queue.length) {
+            let {row, column} = this.queue.pop();
+            for (let j = 0; j < 4; j++) {
+                let newRow = row + this.dx[j];
+                let newColumn = column + this.dy[j];
+                if (this._is_valid(grid, newRow, newColumn)) {
+                    grid[row][column] = 0;
+                    this.queue.push({row: newRow, column: newColumn});
+                }
+            }
+        }
+    }
+    _is_valid(grid, row, column) {
+        if (
+            row < 0 || row >= grid.length ||
+            column < 0 || column >= grid[0].length
+        ) {
+            return false;
+        }
+        for (let i of this.visited) {
+            if (
+                Number(grid[row][column]) === 0 ||
+                (i.row === row && i.column === column)
+            ) {
+                return false;
+            }
+        }
+        return true
+    }
+    _get_result(grid) {
+        let sum = 0;
+        for (let i = 0, row = grid.length; i < row; i++) {
+            for (let j = 0, column = grid[i].length; j < column; j++) {
+                sum += Number(grid[i][j]);
+            }
+        }
+        return sum;
     }
 }
+
+var grid = [
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","1"]
+];
+// var grid = [[1,1,0],
+// [1,1,0],
+// [0,0,1]]
+var a = new Solution();
+console.log(a.numIslands(grid))
--- 数据结构与算法/极客时间/并查集/岛屿/python.py
+++ 数据结构与算法/极客时间/并查集/岛屿/python.py
@@ -3,4 +3,23 @@ class Solution:
         super().__init__()
 
     def numIslands(self, grid):
+        if not grid or not grid[0]:
+            return 0
+        self.max_x = len(grid); self.max_y = len(grid[0]); self.grid = grid
+        self.visited = set()
+        return sum([self.floodfill_dfs(i, j) for i in range(max_x) for j in range(max_y)])
     
+    def floodfill_dfs(self, x, y):
+        if not self._is_vaild(x, y):
+            return 0
+        self.visited.add((x, y))
+        for k in range(4):
+            self.floodfill_dfs(x + self.dx[k], y + self.dy[k])
+        return 1
+
+    def _is_vaild(self, x, y):
+        if x < 0 or x > self.max_x or y <0 or y > self.max_y:
+            return False
+        if (x, y) in self.visited or self.grid[x][y] == '0':
+            return False
+        return True
