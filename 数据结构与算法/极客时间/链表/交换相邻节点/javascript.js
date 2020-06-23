549063ad0b6d375a7ebbb348147472ba09723ec3
yunaichun <yunaichun@qingting.fm>
2020-06-23 14:56:58 +0800

    链表两两交换

--- 数据结构与算法/极客时间/链表/交换相邻节点/javascript.js
+++ 数据结构与算法/极客时间/链表/交换相邻节点/javascript.js
@@ -1,22 +1,27 @@
-import ListNode from '../实现/链表'
 // == leetcode: https://leetcode.com/problems/swap-nodes-in-pairs/
 class Solution {
     constructor() {
     }
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    // == Given 1->2->3->4, you should return the list as 2->1->4->3.
     swapPairs(head) {
-        // == 从头节点开始，同时保存头节点
-        let result = prev = new ListNode(0)
-        prev.next = head
+        // == prev 是记录位置使用，是每一组 2 个当中的最后一个节点
+        // == 实际交换的是 prev.next 和 prev.next.next
+        let prev = self;
+        prev.next = head;
         while (prev.next && prev.next.next) {
-            let a = prev.next
-            let b = prev.next.next
-            prev.next = b
-            a.next = b.next
-            b.next = a
+            let a = prev.next;
+            let b = prev.next.next;
+            // == 交换从起始位置开始
+            prev.next = b;
+            a.next = b.next;
+            b.next = a;
             // ==  重置起点
-            prev = a
+            prev = a;
         }
-        
-        return result.next;
+        return prev.next;
     }
 }
