2a7bfe19d9d78f04432a84e23d2e7d3c34ff1a6c
yunaichun <yunaichun@qingting.fm>
2020-12-18 01:01:30 +0800

    feat: modify

---
+++ JS基础/对象的深拷贝.js
@@ -0,0 +1,45 @@
+// == 利用 ES6 的对象扩展运算符只能实现浅拷贝
+const obj = { a: { b: 1}};
+const obj2 = { ...obj };
+
+// == Object.assign实现深拷贝
+const obj3 = Object.assign(obj);
+
+// == 递归实现深拷贝
+function extendDeep(parent, child) {
+  if (!child) child = Array.isArray(parent[key]) ? [] : {};
+  for (let key in parent) {
+      if (parent.hasOwnProperty(key)) {
+          if (typeof parent[key] === 'object') {
+              child[key] = extendDeep(parent[key], Array.isArray(parent[key]) ? [] : {});
+          } else {
+              child[key] = parent[key];
+          }
+      }
+  }
+  
+  return child;
+}
+
+let parent = {
+  counts: [1, 2, 3],
+  reads: { paper: true }
+};
+let child = extendDeep(parent, {counts: [], c: 1});
+child.counts.push(4);
+console.log(child.counts, parent.counts);
+
+child.reads.english = true;
+console.log(child.reads, parent.reads);
+
+
+// == Object.assign 只能实现浅拷贝
+const obj = { a: { b: 1}};
+const obj2 = Object.assign(obj);
+obj2.a.c = 1;
+console.log(obj);
+
+// == 利用 对象扩展运算符 只能实现浅拷贝
+const obj3 = { ...obj };
+obj3.a.c = 1;
+console.log(obj);
--- JS进阶/原理实现/new关键字实现.js
+++ JS进阶/原理实现/new关键字实现.js
@@ -1,38 +1,36 @@
-/**
- * [fakeNew 模拟实现new]
- * @param  {[Object]} object [要new操作实例的对象]
- * @return {[type]}          [返回实例的对象]
- */
+// == 1、new 原理
 function fakeNew(object) {
+    // == 1. 创建对象，同时设置该对象的 __proto__ 属性
+    let obj = Object.create(object.prototype);
+    // == 等价于
 	// == let obj = {};
 	// == obj.__proto__ = object.prototype;
-	// == object.call(obj)
-	let obj = Object.create(object.prototype);
-	let k = object.call(obj);
+    
+    // == 2、执行构造函数中的代码（为这个对象添加属性和方法）
+    let k = object.call(obj);
+    // == 3、返回对象
 	if (typeof k === 'object') {
 		return k;
-	} else {
-		return obj;
-	}
+    }
+    return obj;
 }
-
-
-
 
 let M = function(name) {
 	this.name = name;
 }
 let obj = fakeNew(M);
-obj instanceof M;
+
+console.log(obj instanceof M);
 
 
+// == 2、instanceof 原理
+const A = function() {}
+const a = new A();
 
+// == a instanceof A 等价于 a.__proto__ === A.prototype
+console.log(a instanceof A);
+console.log(a.__proto__ === A.prototype);
+console.log(A.prototype.__proto__ === Object.prototype);
 
-// == 三、instanceof的原理
-// 1、a instanceof A
-//      等价于
-//      a.__proto__ === A.prototype
-// 2、以此类推（原型对象往上找）
-//      a.__proto__.__proto__ === A.prototype.__proto__
-//      所以 a instanceof Object
-// 3、判断实例是哪个构造函数实例的：a.construtor
+// == 判断实例是哪个构造函数实例的：
+console.log(a.constructor === A);
