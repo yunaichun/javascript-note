b5ac643a5e28e5895d07ccaf40d1517b71af149c
yunaichun <yunaichun@qingting.fm>
2020-12-27 00:21:32 +0800

    feat: 算法-二分查找

--- 数据结构与算法/极客时间/二分查找/Math.sqrt.js
+++ 数据结构与算法/极客时间/二分查找/Math.sqrt.js
@@ -8,9 +8,9 @@ class Solution {
         let max = x;
         while (true) {
             let mid = Math.floor((min + max)/2)
-            if (mid*mid < x) {
+            if (mid * mid < x) {
                 min = mid;
-            } else if (mid*mid > x) {
+            } else if (mid * mid > x) {
                 max = mid;
             } else {
                 return mid;
