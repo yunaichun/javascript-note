2333bc5ba1e5f473f790a820602b29d627cd2008
yunaichun <yunaichun@qingting.fm>
2020-12-28 16:18:18 +0800

    feat: 编辑距离等

--- 数据结构与算法/极客时间/动态规划/三角形最小路径和/javascript.js
+++ 数据结构与算法/极客时间/动态规划/三角形最小路径和/javascript.js
@@ -7,14 +7,16 @@ class Solution {
     // == 初始状态：a[m - 1] = triangle[triangle.length - 1]
     // == 求 a[0][0]
     minimumTotal(triangle) {
-        let m = triangle.length;
         let a = [];
-        a[m - 1] =  triangle[m - 1];
-        for (let i = m - 2; i > -1; i--) {
-            let currentLine = triangle[i];
-            a[i] = [];
-            for (let j = 0, len = currentLine.length; j < len; j++) {
-                a[i][j] = Math.min(a[i + 1][j] , a[i + 1][j + 1]) + currentLine[j];
+        const level = triangle.length;
+        // == 第 level 层
+        a[level - 1] = triangle[level - 1];
+        // == 根据第 level 层推导出第 level - 1 层
+        for (let i = level - 2; i > -1; i--) {
+            const currentLevel = triangle[i];
+            if(!a[i]) a[i] = [];
+            for (let j = 0, len = currentLevel.length; j < len; j++) {
+                a[i][j] = Math.min(a[i + 1][j] , a[i + 1][j + 1]) + currentLevel[j];
             }
         }
         return a[0][0];
