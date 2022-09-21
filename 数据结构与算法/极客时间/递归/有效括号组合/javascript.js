eed5743c2af2f67beb92b7fd3f4783361f164cd2
yunaichun <yunaichun@qingting.fm>
2022-09-21 18:20:01 +0800

    feat(NoTicket): update generateParenthesis

--- 数据结构与算法/极客时间/递归/有效括号组合/javascript.js
+++ 数据结构与算法/极客时间/递归/有效括号组合/javascript.js
@@ -1,22 +1,21 @@
-// == leetcode: https://leetcode.com/problems/generate-parentheses
-class Solution {
-    constructor() {
-    }
-    generateParenthesis(n) {
-        let result = this._helper(0, 0, n, "", []);
-        return result;
-    }
-    _helper(leftUsed, rightUsed, n, current = "", result = []) {
-        if (leftUsed === n && rightUsed === n) {
-            result.push(current);
-            return;
-        }
-        if (leftUsed < n) {
-            this._helper(leftUsed + 1, rightUsed, n, current + '(', result);
-        }
-        if (rightUsed < n && rightUsed < leftUsed) {
-            this._helper(leftUsed, rightUsed + 1, n, current + ')', result)
-        }
-        return result;
-    }
+/** https://leetcode.com/problems/generate-parentheses
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+  return _helper(0, 0, n, '', []);
+};
+
+function _helper(leftUsed, rightUsed, n, current, result) {
+  if (leftUsed === n && rightUsed === n) {
+    result.push(current);
+    return;
+  }
+  if (leftUsed < n) {
+    _helper(leftUsed + 1, rightUsed, n, current + '(', result);
+  }
+  if (rightUsed < n && rightUsed < leftUsed) {
+    _helper(leftUsed, rightUsed + 1, n, current + ')', result);
+  }
+  return result;
 }
