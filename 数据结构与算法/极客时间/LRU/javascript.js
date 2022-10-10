bfbc4e7655a24e9f19fa49a5cb4a5ef46e36f8d8
yunaichun <yunaichun@qtfm.cn>
2022-10-10 22:10:29 +0800

    feat(NoTicket): update LRU

--- 数据结构与算法/极客时间/LRU/javascript.js
+++ 数据结构与算法/极客时间/LRU/javascript.js
@@ -1,33 +1,57 @@
-// == leetcode: https://leetcode.com/problems/lru-cache/
-class LRUCache {
-    // == 存储最大长度
-    constructor(capacity) {
-        this.cache = new Map();
-        this.capacity = capacity;
-    }
-    // == 获取 key 的 value
-    get(key) {
-        if(!this.cache.has(key)) return -1;
-        // == find key and put first
-        const value = this.cache.get(key);
-        this.cache.delete(key);
-        this.cache.set(key, value);
-        return value;
-    }
-    // == 设置 key 和 value
-    put(key, value) {
-        if (this.cache.get(key)) this.cache.delete(key);
-        this.cache.set(key, value);
-        if(this.cache.size > this.capacity) {
-            const keys = this.cache.keys();
-            const firstKey = keys.next().value;
-            this.cache.delete(firstKey);
-        }
-    }
-}
+// https://leetcode.cn/problems/lru-cache/
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+  this.capacity = capacity;
+  this.map = new Map();
+};
+
 /** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  if (!this.map.has(key)) return -1;
+  const value = this.map.get(key);
+  /** 一旦取值就放在最后面 */
+  this.map.delete(key);
+  this.map.set(key, value);
+  return value;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  if (this.map.get(key)) this.map.delete(key);
+  this.map.set(key, value);
+  /** 删除先进来的，刚添加进来最后删除 */
+  if (this.map.size > this.capacity) {
+    const keys = this.map.keys();
+    const firstKey = keys.next().value;
+    this.map.delete(firstKey);
+  }
+};
+
+/**
  * Your LRUCache object will be instantiated and called as such:
  * var obj = new LRUCache(capacity)
  * var param_1 = obj.get(key)
- * obj.put(key, value)
+ * obj.put(key,value)
  */
+
+
+const lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // 缓存是 {1=1}
+lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+lRUCache.get(1);    // 返回 1
+lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+lRUCache.get(2);    // 返回 -1 (未找到)
+lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+lRUCache.get(1);    // 返回 -1 (未找到)
+lRUCache.get(3);    // 返回 3
+lRUCache.get(4);    // 返回 4
