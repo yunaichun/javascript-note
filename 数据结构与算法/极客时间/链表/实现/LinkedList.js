0a421f37c66754d9ac2c12a8b860faff22a70c62
yunaichun <yunaichun@qtfm.cn>
2022-10-12 08:13:07 +0800

    feat(NoTicket): update linklist

--- 数据结构与算法/极客时间/链表/实现/LinkedList.js
+++ 数据结构与算法/极客时间/链表/实现/LinkedList.js
@@ -1,42 +1,38 @@
-class Node {
-    constructor(val = null, next = null) {
-        this.val = val
-        this.next = next
-    }
+function Node(data = null, next = null) {
+  this.data = data;
+  this.next = next;
 }
 
-class ListNode {
-    constructor() {
-        this.head = new Node();
+function ListNode() {
+  this.head = new Node();
+}
+ListNode.prototype.insertAfter = function (sourceNode, targetNode) {
+  let first = this.head;
+  if (first.data === null && first.next === null) {
+    this.head = sourceNode;
+  } else {
+    sourceNode.next = targetNode.next;
+    targetNode.next = sourceNode;
+  }
+  return first;
+}
+ListNode.prototype.removeNode = function (node) {
+  let first = this.head;
+  /** 找到 node 节点的父节点 */
+  while (first.next) {
+    if (first.next === node) {
+      first.next = first.next.next;
+      break;
     }
-
-    insertAfter(sourceNode, targetNode) {
-        if (this.head.val === null && this.head.next === null) {
-            this.head = sourceNode
-        } else {
-            sourceNode.next = targetNode.next
-            targetNode.next = sourceNode
-        }
-        return this.head
-    }
-    
-    removeNode(node) {
-        let first = this.head;
-        while(first.next) {
-            if (first.next === node) {
-                first.next = first.next.next
-                break
-            }
-            first = first.next
-        }
-        return this.head
-    }
+    first = first.next;
+  }
+  return first;
 }
 
-let a = new Node(1)
-let b = new Node(2)
-let c = new Node(3)
-let list = new ListNode(a)
+var a = new Node(1)
+var b = new Node(2)
+var c = new Node(3)
+var list = new ListNode(a)
 list.insertAfter(a)
 list.insertAfter(b, a)
 list.insertAfter(c, b)
