6a366f67f8c711e21b0c53fd920fb10d32a4d932
yunaichun <yunaichun@qingting.fm>
2022-10-13 17:29:52 +0800

    feat(NoTicket): update hash

---
+++ 数据结构与算法/极客时间/哈希表/有效字母异同位/javascript.js
@@ -0,0 +1,8 @@
+/** https://leetcode.cn/problems/valid-anagram/
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  return s.split("").sort().join() === t.split("").sort().join();
+};
