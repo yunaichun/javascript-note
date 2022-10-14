07737c4eea83069455eae5170511f08d75844d2f
yunaichun <yunaichun@qingting.fm>
2022-10-14 16:22:33 +0800

    feat(NoTicket): update pow

--- 数据结构与算法/极客时间/动态规划/股票买卖/手续费/javascript.js
+++ 数据结构与算法/极客时间/动态规划/股票买卖/手续费/javascript.js
@@ -1,8 +1,6 @@
-// == leetcode - 714: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+// == leetcode - 714: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
 class Solution {
-    constructor() {
-    }
-    // == 含有交易手续费
-    maxProfit(prices) {
-    }
+  constructor() {}
+  // == 含有交易手续费
+  maxProfit(prices) {}
 }
--- 数据结构与算法/极客时间/递归/n次幂/pow(x,n).js
+++ 数据结构与算法/极客时间/递归/n次幂/pow(x,n).js
@@ -3,15 +3,10 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function(x, n) {
+var myPow = function (x, n) {
   if (n < 0) return 1 / myPow(x, -n);
-  if (n === 0) return  1;
+  if (n === 0) return 1;
   if (n === 1) return x;
-  const a = Math.floor(n / 2);
-  const b = n % 2;
-  if (b === 0) {
-    return myPow(x*x, a);
-  } else {
-    return myPow(x*x, a) * x;
-  }
+  if (n % 2) return myPow(x * x, (n - 1) / 2) * x;
+  return myPow(x * x, n / 2);
 };
