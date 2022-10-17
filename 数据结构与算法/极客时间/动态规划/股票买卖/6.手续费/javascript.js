4ff4576112e8d1b40b8d01ccf75d109e1b1e881d
yunaichun <yunaichun@qingting.fm>
2022-10-17 14:05:50 +0800

    feat(NoTicket): update maxProfit

--- 数据结构与算法/极客时间/动态规划/股票买卖/6.手续费/javascript.js
+++ 数据结构与算法/极客时间/动态规划/股票买卖/6.手续费/javascript.js
@@ -5,4 +5,15 @@
  * @param {number} fee
  * @return {number}
  */
-var maxProfit = function (prices, fee) {};
+var maxProfit = function (prices, fee) {
+  const dp = [[0, -prices[0]]];
+  /** dp[i][0] 代表第 i + 1 天 【不持有股票】 最大收益 */
+  /** dp[i][1] 代表第 i + 1 天 【持有股票】 最大收益 */
+  for (let i = 1, len = prices.length; i < len; i += 1) {
+    dp[i] = [
+      Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee),
+      Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]),
+    ];
+  }
+  return dp[prices.length - 1][0];
+};
