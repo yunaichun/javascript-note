20b4c3400b8dc7c3ac2a57caa4f35f86e1a5706e
yunaichun <yunaichun@qingting.fm>
2022-10-17 17:49:27 +0800

    feat(NoTicket): update dynamic

--- 数据结构与算法/极客时间/动态规划/最长上升子序列/javascript.js
+++ 数据结构与算法/极客时间/动态规划/最长上升子序列/javascript.js
@@ -7,7 +7,7 @@
 var lengthOfLIS = function (nums) {
   /** dp[i] 代表【以数组第 i + 1 个数结尾的最长严格递增子序列】的长度 */
   const dp = [1];
-  for (let i = 0, len = nums.length; i < len; i += 1) {
+  for (let i = 1, len = nums.length; i < len; i += 1) {
     if (!dp[i]) dp[i] = 1;
     for (let j = 0; j < i; j += 1) {
       if (nums[i] > nums[j]) dp[i] = Math.max(dp[j] + 1, dp[i]);
