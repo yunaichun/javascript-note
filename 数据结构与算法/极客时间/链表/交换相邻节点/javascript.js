3caa09d7bfa275ec67e186783cfdb47254cf7837
yunaichun <yunaichun@qingting.fm>
2022-10-24 13:47:03 +0800

    feat(NoTicket): update linked list

--- 数据结构与算法/极客时间/链表/交换相邻节点/javascript.js
+++ 数据结构与算法/极客时间/链表/交换相邻节点/javascript.js
@@ -1,32 +1,48 @@
+/** https://leetcode.cn/problems/swap-nodes-in-pairs */
+
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/** https://leetcode.cn/problems/swap-nodes-in-pairs/
  * @param {ListNode} head
  * @return {ListNode}
  */
 var swapPairs = function (head) {
-  // 1 -> 2 -> 3 -> 4
-  // 1、0(cur) -> 2 -> 1
-  // 2、0 -> 2 -> 1(cur) -> 4 -> 3
-  const dummyHead = new ListNode(0);
-  dummyHead.next = head;
-  let cur = dummyHead;
-  while (cur.next && cur.next.next) {
-    /** 控制循环: 下一个节点 */
-    const node1 = cur.next; // 1
-    const node2 = cur.next.next; // 2
+  const dynamicHead = new ListNode(0);
+  dynamicHead.next = head;
 
-    cur.next = node2; // 0.next = 2
-    node1.next = node2.next; // 1.next = 2.next
-    node2.next = node1;
+  let headPre = dynamicHead;
+  while (head) {
+    let tail = headPre;
+    /** 1、判断是否包含 2 个元素 */
+    for (let i = 0; i < 2; i += 1) {
+      tail = tail.next;
+      if (!tail) return dynamicHead.next;
+    }
 
-    /** 控制循环: 下一个节点 */
-    cur = node1;
+    /** 2、交换 2 个节点 */
+    const temp = tail.next;
+    [head, tail] = reverse(head, tail);
+
+    /** 3、拼接 */
+    headPre.next = head;
+    tail.next = temp;
+
+    /** 下一个循环 */
+    headPre = tail;
+    head = temp;
   }
-  return dummyHead.next;
+
+  return dynamicHead.next;
+};
+
+var reverse = function (head, tail) {
+  let prev = null;
+  let cur = head;
+  while (cur && prev !== tail) {
+    const temp = cur.next;
+
+    cur.next = prev;
+    prev = cur;
+
+    cur = temp;
+  }
+  return [tail, head];
 };
