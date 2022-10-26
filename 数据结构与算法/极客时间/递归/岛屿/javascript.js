aebf59da8f6155d6125533a804af52d4b848c88d
yunaichun <yunaichun@qingting.fm>
2022-10-26 16:21:27 +0800

    feat(NoTicket): update dynamic

--- 数据结构与算法/极客时间/递归/岛屿/javascript.js
+++ 数据结构与算法/极客时间/递归/岛屿/javascript.js
@@ -2,41 +2,40 @@
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
 
-/** https://leetcode.cn/problems/number-of-islands/
+/**
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function(grid) {
+var numIslands = function (grid) {
   let sum = 0;
-  for (let i = 0, row = grid.length; i < row; i += 1) {
-    for (let j = 0, column = grid[i].length; j < column; j += 1) {
-      if (Number(grid[i][j]) === 1) {
+  for (let i = 0, len1 = grid.length; i < len1; i += 1) {
+    for (let j = 0, len2 = grid[i].length; j < len2; j += 1) {
+      if (grid[i][j] === "1") {
         _helper(grid, i, j, [{ x: i, y: j }]);
-        sum++;
+        sum += 1;
       }
     }
   }
-  console.log(grid);
+  console.log(123, grid);
   return sum;
 };
 
-var _helper = function(grid, row, column, visited) {
-  let m = grid.length;
-  let n = grid[0].length;
-  for (let i = 0; i < 4; i++) {
-    let x = row + dx[i];
-    let y = column + dy[i];
-    const inVisited = visited.find(i => i.x === x && i.y === y);
-    const inBoard = x >= 0 && x < m && y >= 0 && y < n;
-    if (!inVisited && inBoard) {
-      if (Number(grid[x][y]) === 1) {
-        /** 开始消 1 附近（上下左右）的 1 */
+var _helper = function (grid, row, column, visited) {
+  const m = grid.length;
+  const n = grid[0].length;
+  for (let i = 0; i < 4; i += 1) {
+    const x = dx[i] + row;
+    const y = dy[i] + column;
+    const inGrid = x >= 0 && x < m && y >= 0 && y < n;
+    const inVisited = visited.find((i) => i.x === x && i.y === y);
+    if (inGrid && !inVisited) {
+      if (grid[x][y] === "1") {
         grid[x][y] = 0;
         _helper(grid, x, y, visited.concat({ x, y }));
       }
     }
   }
-}
+};
 
 // grid = [
 //   ["1","1","1","1","0"],
@@ -46,8 +45,8 @@ var _helper = function(grid, row, column, visited) {
 // ];
 
 grid = [
-  ["1","1","1"],
-  ["0","1","0"],
-  ["0","1","0"]
+  ["1", "1", "1"],
+  ["0", "1", "0"],
+  ["0", "1", "0"],
 ];
 console.log(numIslands(grid));
