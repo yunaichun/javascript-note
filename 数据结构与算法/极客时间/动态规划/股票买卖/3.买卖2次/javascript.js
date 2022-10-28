76f9438385a04fba5334d9d940ddf6a2d272a96a
yunaichun <yunaichun@qingting.fm>
2022-10-28 17:00:22 +0800

    feat(NoTicket): update

--- 数据结构与算法/极客时间/动态规划/股票买卖/3.买卖2次/javascript.js
+++ 数据结构与算法/极客时间/动态规划/股票买卖/3.买卖2次/javascript.js
@@ -5,12 +5,12 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  const dp = [[0, -prices[0], 0, -prices[0], 0]];
   /** dp[i][0] 代表第 i + 1 天 【不做任何操作】 最大收益 */
   /** dp[i][1] 代表第 i + 1 天状态为 【第一次买入】 最大收益 */
   /** dp[i][2] 代表第 i + 1 天状态为 【第一次卖出】 最大收益 */
   /** dp[i][3] 代表第 i + 1 天状态为 【第二次买入】 最大收益 */
   /** dp[i][4] 代表第 i + 1 天状态为 【第二次卖出】 最大收益 */
+  const dp = [[0, -prices[0], 0, -prices[0], 0]];
   for (let i = 1, len = prices.length; i < len; i += 1) {
     dp[i] = [
       dp[i - 1][0],
--- 数据结构与算法/极客时间/动态规划/股票买卖/5.冻结1天/javascript.js
+++ 数据结构与算法/极客时间/动态规划/股票买卖/5.冻结1天/javascript.js
@@ -5,9 +5,9 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
+  /** dp[i][0] 代表第 i + 1 天 【不持有股票】 最大收益 */
+  /** dp[i][1] 代表第 i + 1 天 【持有股票】 最大收益 */
   const dp = [[0, -prices[0]]];
-  /** dp[i][0] 代表第 i 天 【不持有股票】 最大收益 */
-  /** dp[i][1] 代表第 i 天 【持有股票】 最大收益 */
   for (let i = 1, len = prices.length; i < len; i += 1) {
     dp[i] = [
       Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]),
