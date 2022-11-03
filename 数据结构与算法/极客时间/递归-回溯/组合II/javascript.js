faa950251022e096dbb8911dddb0dc8144e31060
yunaichun <yunaichun@qingting.fm>
2022-11-03 13:55:20 +0800

    feat(NoTicket): update

--- 数据结构与算法/极客时间/递归-回溯/组合II/javascript.js
+++ 数据结构与算法/极客时间/递归-回溯/组合II/javascript.js
@@ -13,21 +13,24 @@ var combinationSum = function (candidates, target) {
 };
 
 var _helper = function (candidates, target, path, results) {
+  /** 2、保存结果: 终止条件 */
   if (target < 0) return;
-  /** 2、把每一条路径加入结果集: 判断是否有结束条件 */
   if (target === 0) {
     const exist = results.find((i) => i.sort().join() === path.sort().join());
     if (!exist) results.push(path);
     return;
   }
+
   for (let i = 0, len = candidates.length; i < len; i += 1) {
+    /** 3、选择+递归+重置: 剪枝 */
     const current = candidates[i];
     path.push(current);
+    /** 不能再终止条件的时候再做累加，多余步骤: 每次选择后 target 越来越少，相当于做了累加操作 */
     _helper(candidates, target - current, [...path], results);
     path.pop();
   }
 };
 
-// (candidates = [2, 3, 6, 7]), (target = 7);
-(candidates = [2, 3, 5]), (target = 8);
+candidates = [8, 6, 4, 12, 5, 7, 3, 11];
+target = 28;
 console.log(combinationSum(candidates, target));
--- 数据结构与算法/极客时间/递归-回溯/组合III/javascript.js
+++ 数据结构与算法/极客时间/递归-回溯/组合III/javascript.js
@@ -14,17 +14,19 @@ var combinationSum2 = function (candidates, target) {
 };
 
 var _helper = function (candidates, target, start, path, results) {
+  /** 2、保存结果: 终止条件 */
   if (target < 0) return;
-  /** 2、把每一条路径加入结果集: 判断是否有结束条件 */
   if (target === 0) {
     const exist = results.find((i) => i.sort().join() === path.sort().join());
     if (!exist) results.push(path);
     return;
   }
   for (let i = start, len = candidates.length; i < len; i += 1) {
+    /** 3、选择+递归+重置: 剪枝 */
     const current = candidates[i];
     if (i > start && candidates[i] == candidates[i - 1]) continue;
     path.push(current);
+    /** 不能再终止条件的时候再做累加，多余步骤: 每次选择后 target 越来越少，相当于做了累加操作 */
     _helper(candidates, target - current, i + 1, [...path], results);
     path.pop();
   }
